855dfc0272b11c653968156fb9e7b4bb66786836
Pietro Grandi <pietro.grandi.3d@gmail.com>
2022-02-16 18:42:49 +0100

    chapter 4

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const { ApolloServer, gql } = require('apollo-server-express');
-const { cars, users } = require('./data');
+let cars = require('./data').cars;
+let users = require('./data').users;
 const me = users[0];
 
 const typeDefs = gql`
@@ -9,20 +10,29 @@ const typeDefs = gql`
         me: User
         users: [User]!
         user(id: ID!): User
-        cars: [Car]!
+        cars: [Car!]!
         car(id: ID!): Car
+    }
+
+    type Mutation {
+        makeUser(name: String!): User!
+        removeUser(id: ID!): Boolean
+        makeCar(make: String!, model: String!, color: String!, ownedBy: ID!): Car!
+        removeCar(id: ID!): Boolean
     }
 
     type User {
         id: ID!
         name: String!
+        cars: [Car!]!
     }
 
     type Car {
         id: ID!
-        maker: String!
+        make: String!
         model: String!
         color: String!
+        owner: User!
     }
 `;
 
@@ -33,7 +43,49 @@ const resolvers = {
         user: (_parent, arguments, _context, _info) =>
             users.find((user) => user.id === arguments.id),
         cars: () => cars,
-        car: (_parent, arguments, _context, _info) => cars.find((user) => user.id === arguments.id),
+        car: (_parent, arguments, _context, _info) => cars.find(({ id }) => id === arguments.id),
+    },
+    Mutation: {
+        makeUser: (parent, { name }) => {
+            const nextId = parseInt(users[users.length - 1].id, 10) + 1;
+            const newUser = { id: nextId, name, cars: [] };
+            users.push(newUser);
+            return newUser;
+        },
+        removeUser: (parent, { id }) => {
+            let found = false;
+            users = users.filter((user) => {
+                if (user.id === id) {
+                    found = true;
+                    return false;
+                }
+                return true;
+            });
+            return found;
+        },
+        makeCar: (parent, { model, make, color, ownedBy }) => {
+            const nextId = parseInt(cars[cars.length - 1].id, 10) + 1;
+            const newCar = { id: nextId, model, make, color, ownedBy };
+            cars.push(newCar);
+            return newCar;
+        },
+        removeCar: (parent, { id }) => {
+            let found = false;
+            cars = cars.filter((car) => {
+                if (car.id === id) {
+                    found = true;
+                    return false;
+                }
+                return true;
+            });
+            return found;
+        },
+    },
+    Car: {
+        owner: (parent) => users.find(({ id }) => id === parent.ownedBy),
+    },
+    User: {
+        cars: (parent) => parent.cars.map((carId) => cars.find(({ id }) => id === carId)),
     },
 };
 
--- data.js
+++ data.js
@@ -2,20 +2,22 @@ let users = [
     {
         id: '1',
         name: 'Blu',
+        cars: ['1', '2'],
     },
-    { id: '2', name: 'Zeno' },
-    { id: '3', name: 'Romeo' },
+    { id: '2', name: 'Zeno', cars: ['3'] },
+    { id: '3', name: 'Romeo', cars: [] },
 ];
 
 let cars = [
     {
         id: '1',
-        maker: 'Fiat',
+        make: 'Fiat',
         model: 'Panda',
         color: 'blue',
+        ownedBy: '1',
     },
-    { id: '2', maker: 'Renault', model: 'Clio', color: 'dark green' },
-    { id: '3', maker: 'Fiat', model: 'Tipo', color: 'red' },
+    { id: '2', make: 'Renault', model: 'Clio', color: 'dark green', ownedBy: '1' },
+    { id: '3', make: 'Fiat', model: 'Tipo', color: 'red', ownedBy: '2' },
 ];
 
 module.exports = { users, cars };
