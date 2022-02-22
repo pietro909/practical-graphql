0192fcee1f77369cb3ab955c394e08d7f76072c9
Pietro Grandi <pietro.grandi.3d@gmail.com>
2022-02-22 18:26:42 +0100

    handle loading

--- client/src/User.js
+++ client/src/User.js
@@ -7,9 +7,32 @@ export default class User extends Component {
         return (
             <div>
                 <Query query={query}>
-                    {({ data }) => {
-                        console.log(data);
-                        return null;
+                    {({ data , loading}) => {
+                        if (loading) {
+                            return <p>Loading...</p>
+                        }
+                        return (
+                            <div>
+                                <ul>
+                                    {data.users.map(({ id, name, cars }) => (
+                                        <li key={id}>
+                                            {name}
+                                            <ul>
+                                                {cars.length !== 0 ? (
+                                                    cars.map(({ id, make, model, color }) => (
+                                                        <li key={`${id}-${make}`}>
+                                                            {make} {model} {color}
+                                                        </li>
+                                                    ))
+                                                ) : (
+                                                    <li>{'No cars'}</li>
+                                                )}
+                                            </ul>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        );
                     }}
                 </Query>
             </div>
